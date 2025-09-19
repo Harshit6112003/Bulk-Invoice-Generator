ae0818febbaf7a7dad4610f1b87ccd1d456d2fa0
Harshit6112003 <harshit11fcsharma@gmail.com>
2025-09-19 12:22:29 +0530

    first commit

--- Backend/utils/whatsappSender.js
+++ Backend/utils/whatsappSender.js
@@ -1,12 +1,12 @@
-const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+// const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
-const sendWhatsApp = async (to, invoice) => {
-  const message = `Hi ${invoice.name}, your invoice of ₹${invoice.total} is ready.`;
-  await client.messages.create({
-    body: message,
-    from: 'whatsapp:+14155238886',
-    to: `whatsapp:${to}`,
-  });
-};
+// const sendWhatsApp = async (to, invoice) => {
+//   const message = `Hi ${invoice.name}, your invoice of ₹${invoice.total} is ready.`;
+//   await client.messages.create({
+//     body: message,
+//     from: 'whatsapp:+14155238886',
+//     to: `whatsapp:${to}`,
+//   });
+// };
 
-module.exports = sendWhatsApp;
+// module.exports = sendWhatsApp;
